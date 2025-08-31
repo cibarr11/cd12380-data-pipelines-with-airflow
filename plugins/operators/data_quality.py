7a963641a0a791509693a0b134d49575fdd947e7
cei7 <ibarcarl@oregonstate.edu>
2025-08-30 18:03:50 -0700

    commit - added connection id variable to init function, added quality check for empty rows or mismatched row counts

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -11,12 +11,33 @@ class DataQualityOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
-                 conn_id, table,*args, **kwargs):
+                  redshift_conn_id="redshift", tables=[],*args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        # Added redshift connection id for reference
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
+        # Example logic (to be replaced with actual data quality checks)
         self.log.info('DataQualityOperator not implemented yet')
+
+        # verify for failures
+        for table in self.tables:
+            # define empty list to store the failures
+            failures = []
+            # failure to verify is the table is not empty
+            verify_table = hook.get_first(f"SELECT COUNT(*) FROM {table}")
+
+            if verify_table is None or len(verify_table) == 0:
+                failures.append(f"{table}: no data")
+            elif verify_table[0] is None or verify_table[0] <= 0:
+                failures.append(f"{table}: incorrect number of rows")
+        
+        if failures:
+            raise ValueError("Failures found")
+        else:
+            self.log.info("All data quality checks passed.")
